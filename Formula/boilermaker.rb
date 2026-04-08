3127c4c0ab19b802c34e7aeafcf4889db2fee3ad
Arnaud Berthomier <oz@cyprio.net>
2026-04-07 19:39:50 -0600

    Release v0.1.0-rc0

--- Formula/boilermaker.rb
+++ Formula/boilermaker.rb
@@ -3,28 +3,32 @@ class Boilermaker < Formula
   homepage "https://github.com/yeajustmars/boilermaker"
   license "MIT"
 
-  version 'v0.1.0-beta5'
+  version 'v0.1.0-rc0'
 
   on_arm do
     on_macos do
       url "https://github.com/yeajustmars/boilermaker/releases/download/#{version}/boilermaker_cli-aarch64-apple-darwin.tar.xz"
-      sha256 "90115a792ed3924bcf9492d37420a59de111a97253b59119347456565eb1c466"
+      sha256 "933cdf6dc6befddfe87f805313129cfc3299f7f6c767c8f84153c21ac206985d "
+
     end
 
     on_linux do
       url "https://github.com/yeajustmars/boilermaker/releases/download/#{version}/boilermaker_cli-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "0f54eba3ed4590f85e173b608af63dca442945a93b348cc7a9d10cffb96d3f57"
+      sha256 "334218412d8dfbc363f48189ceec23f43163009f620dbe9efc6fdd4f435cff6a "
+
     end
   end
 
   on_intel do
     on_macos do
       url "https://github.com/yeajustmars/boilermaker/releases/download/#{version}/boilermaker_cli-x86_64-apple-darwin.tar.xz"
-      sha256 "e80e5f0795223f46e991e20a3e70a9d6834c312cb58686278aca0bfb3869cbad"
+      sha256 "6d6a45d91343e9dcc30044d4b9fec4eea04e0a052cb414b73ef97cf7f9218aff "
+
     end
     on_linux do
       url "https://github.com/yeajustmars/boilermaker/releases/download/#{version}/boilermaker_cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "9e85339a999f42649df955893e6500350a9b3535bb1a14199bcd321021f564e5"
+      sha256 "22df981a1d910a340c23c00dffaf636b3315cde351a76a71fa527ae77ff95e42 "
+
     end
   end
 
@@ -33,6 +37,6 @@ class Boilermaker < Formula
   end
 
   test do
-    assert_equal "boilermaker_cli 0.1.0-beta5\n", shell_output("'#{bin}/boil' -V")
+    assert_equal "boilermaker_cli 0.1.0-rc0\n", shell_output("'#{bin}/boil' -V")
   end
 end
